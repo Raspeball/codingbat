879d07082b771f44f671683390b22c373ec891af
Raspeball <Raspeball@users.noreply.github.com>
2022-05-23 20:26:37 +0200

    warmups continue

---
+++ missing_char.py
@@ -0,0 +1,12 @@
+## https://codingbat.com/prob/p149524 ##
+# missing_char #
+
+'''Given a non-empty string and an int n, return a new string where the char at
+index n has been removed. The value of n will be a valid index of a char in the
+original string (i.e. n will be in the range 0..len(str)-1 inclusive).'''
+
+def missing_char(str, n):
+    return str[0:n] + str[n+1:]
+
+
+print(missing_char("x", 0))
