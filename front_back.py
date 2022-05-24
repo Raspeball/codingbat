bcd325d44ab9f558649c24b4134191e7ad69eb1e
Raspeball <Raspeball@users.noreply.github.com>
2022-05-24 20:58:16 +0200

    codingbat info

--- front_back.py
+++ front_back.py
@@ -1,4 +1,9 @@
 ## https://codingbat.com/prob/p153599 ##
+# front_back #
+
+'''
+Given a string,
+return a new string where the first and last chars have been exchanged.'''
 
 def front_back(str):
     ln = len(str)
