f9dc0de145f81af14d9c4a0b7fb165751dbe961b
Raspeball <Raspeball@users.noreply.github.com>
2022-06-01 22:43:54 +0200

    logic-1 cont and logic-2 startup

---
+++ logic-2/make_chocolate.py
@@ -0,0 +1,26 @@
+## https://codingbat.com/prob/p190859 ##
+# make_chocolate #
+
+'''
+We want make a package of goal kilos of chocolate.
+We have small bars (1 kilo each) and big bars (5 kilos each).
+Return the number of small bars to use,
+assuming we always use big bars before small bars.
+Return -1 if it can't be done.'''
+
+def make_chocolate(small, big, goal):
+
+    res = goal
+
+    if goal // big < 5:
+        #res = res - (big - 1)*5
+        res = goal%5
+
+    else:
+        res = res - big*5
+
+    if res > small:
+        return -1
+
+    else:
+        return res
